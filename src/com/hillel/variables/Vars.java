5d1fb04b814b30c4fc563b974ade14d4611106ea
Annetik <akononenko405@gmail.com>
2016-12-08 20:59:18 +0200

    my second commit

---
+++ src/com/hillel/variables/Vars.java
@@ -0,0 +1,26 @@
+package com.hillel.variables;
+
+public class Vars {
+	public static void main(String[] args){
+	
+	//byte var = 127;
+	//int num = var + 1;
+	
+	//byte anotherVar = (byte) num;
+	
+	
+	//System.out.println(anotherVar);
+	
+	float var = 3.5f;
+	double d = 34.45;
+	
+	int a = 30;
+	int b = 25;
+	
+	double c = a / b;
+	
+	System.out.println(c);
+	
+	
+}
+}
